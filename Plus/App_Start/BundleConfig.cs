19c254f3705dbdd3199199d2eb175d9761ad068e
udhayakumar rangaraju <udhaya.raju@hotmail.com>
2020-04-13 16:18:43 +0100

    Add project files.

---
+++ Plus/App_Start/BundleConfig.cs
@@ -0,0 +1,68 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Plus
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //                        "~/js/jquery-1.11.3.min.js"));
+
+            //            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //                        "~/Scripts/jquery.validate*"));
+
+            //            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            //            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //                        "~/Scripts/modernizr-*"));
+
+            //            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+
+
+            //                        "~/Scripts/bootstrap.js",
+            //     "~/revolution/js/jquery.themepunch.tools.min.js",
+            //     "~/revolution/js/jquery.themepunch.revolution.min.js",
+            //     "~/revolution/js/extensions/revolution.extension.slideanims.min.js",
+            //     "~/revolution/js/extensions/revolution.extension.layeranimation.min.js",
+            //     "~/revolution/js/extensions/revolution.extension.navigation.min.js",
+            //     "~/revolution/js/extensions/revolution.extension.parallax. min.js",
+
+            //    "~/js/bootstrap.min.js",
+            //    "~/js/menuzord.js",
+            //    "~/js/owl.carousel.min.js",
+            //    "~/js/jquery.counterup.js",
+            //    "~/js/waypoints.min.js",
+            //    "~/js/countdown.js",
+            //    "~/js/parallax.min.js",
+            //    "~/js/jquery.bootFolio.js",
+            //    "~/js/jquery.magnific-popup.js",
+
+            //    "~/js/jquery-ui.js",
+            //    "~/js/rev-function.js",
+            //    "~/js/smoothscroll.js",
+            //    "~/js/jquery.matchHeight.js",
+            //    "~/js/main.js"
+
+            //                      ));
+
+//            bundles.Add(new StyleBundle("~/Content").Include(
+//                      "~/css/bootstrap.min.css",
+//"~/css/font-awesome.css",
+//"~/css/menuzord.css",
+//"~/css/stroke-icon.css",
+//"~/css/demo.css",
+//"~/css/ie7.css",
+//"~/css/bootFolio.css",
+//"~/css/magnific-popup.css",
+//"~/css/jquery-ui.css",
+//"~/css/owl.theme.default.css",
+//"~/css/owl.carousel.css",
+//"~/revolution/css/settings.css",
+//"~/revolution/css/layers.css",
+//"~/revolution/css/navigation.css", "~/css/style.css"));
+        }
+    }
+}
---
+++ Plus/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plus.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Aboutus()
+        {
+            return View();
+        }
+
+        public ActionResult Contactus()
+        {
+            return View();
+        }
+        public ActionResult Agencies()
+        {
+            return View();
+        }
+        public ActionResult Faq()
+        {
+            return View();
+        }
+        public ActionResult How()
+        {
+            return View();
+        }
+        public ActionResult Refer()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult WhyUs()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
